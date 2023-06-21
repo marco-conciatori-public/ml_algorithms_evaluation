7b2bbd7f2ac27b317c570f4365348f74178d1213
Marco Conciatori <conciatori.marco91@gmail.com>
2023-06-21 16:35:09 +0900

    added constants

---
+++ global_constants.py
@@ -0,0 +1,11 @@
+# handy aliases
+THRESHOLDS_NORMALIZED = 'thresholds_normalized'
+GLOBAL_MIN_VALUE = 'global_min_val'
+GLOBAL_MAX_VALUE = 'global_max_val'
+
+# CLASSIFICATION METRICS
+PRECISION = 'Precision'
+RECALL = 'Recall'
+F1_SCORE = 'F1_score'
+ACCURACY = 'Accuracy'
+CONFUSION_MATRIX_SHORT_NAME = 'Confusion_matrix'
